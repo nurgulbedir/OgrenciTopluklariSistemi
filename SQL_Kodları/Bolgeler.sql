69f9731b9811827bce3ccfb62be53ab5c2b3054f
Nurgül Bedir <nurgulbedir5@gmail.com>
2025-01-05 21:12:02 +0300

    Delete SQL_Kodları directory

--- SQL_Kodları/Bolgeler.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Bolgeler (B_id, B_ad) VALUES
-(1, 'Marmara Bölgesi'),
-(2, 'Ege Bölgesi'),
-(3, 'Akdeniz Bölgesi'),
-(4, 'Ýç Anadolu Bölgesi'),
-(5, 'Karadeniz Bölgesi'),
-(6, 'Doðu Anadolu Bölgesi'),
-(7, 'Güneydoðu Anadolu Bölgesi');
--- SQL_Kodları/ÜyeSayisiSutununuEkleme.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE OgrenciTopluluklari
-ADD Uye_Sayisi INT DEFAULT 0;
-
-
-
-
