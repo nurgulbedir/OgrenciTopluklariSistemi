50b689f7c8fa3197deb19e63f045e15e563e1699
Nurgül Bedir <nurgulbedir5@gmail.com>
2025-01-05 21:10:35 +0300

    Delete SQL directory

--- SQL/Etkinlikler.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO Etkinlikler (E_id, E_ad, Ot_id) VALUES
-(1, 'Yazýlým Geliþtirme Hackathon', 21), 
-(2, 'Siber Güvenlik Konferansý', 22),        
-(3, 'Kitap Okuma Etkinliði', 23),            
-(4, 'Google Developer Group Meetup', 24),    
-(5, 'Tarih ve Kültür Semineri', 25);          
